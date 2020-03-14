69aa22dd6699979f40bf9a08ee2cc12652ecd8ea
Zawiar <zawiarkhaskheli@gmail.com>
2020-03-14 14:13:51 +0500

    final v2

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use("/routes/api", profileRoute);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname + "/../client/build/index.html"));
   });
 }
 
